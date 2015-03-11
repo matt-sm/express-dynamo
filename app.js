7a78126fee28ebc3af4fcd543a0f8f3db0196966
msmith-bauer <matsmith@bauer-media.com.au>
2015-03-11 21:17:22 +1100

    refactored code into db.js

--- app.js
+++ app.js
@@ -1,18 +1,11 @@
 var express = require('express'),
-mongoose = require('mongoose'),
 bodyParser = require('body-parser');
-
-var mongoUri = 'mongodb://localhost:27017/noderest';
-mongoose.connect(mongoUri);
-var db = mongoose.connection;
-db.on('error', function () {
-  throw new Error('unable to connect to database at ' + mongoUri);
-});
 
 var app = express();
 
 app.use(bodyParser.json());
 
+require('./db');
 require('./models/tag');
 require('./routes')(app);
 
