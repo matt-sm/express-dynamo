c52d1290847d7e454b9bb0d6039a458c597734b6
msmith-bauer <matsmith@bauer-media.com.au>
2015-03-23 17:29:26 +1100

    added another test for findbyid

--- tests/tags-test.js
+++ tests/tags-test.js
@@ -3,8 +3,9 @@ var proxyquire = require('proxyquire'),
     chai = require("chai"),
     expect = chai.expect;
 
-describe('The tags controller', function () {
-    it('should return a tag', function (done) {
+describe('Tags', function () {
+	describe('FindById', function () {
+    it('should return a tag when passed a valid Id', function (done) {
         var res = {},
 		    testId = "Test-Id",
 		    req = { params : { id : testId}}
@@ -22,4 +23,25 @@ describe('The tags controller', function () {
         expect(sendSpy.calledOnce).to.equal(true);
         expect(sendSpy.calledWith(testTag)).to.equal(true);
     })
+})
+})
+
+describe('Tags', function () {
+	describe('FindById', function () {
+    it('should return a 500 when passed an invalid Id', function (done) {
+        var res = err = {},
+		    req = { params : { id : "xxx"}}
+            sendSpy = res.send = sinon.spy(),
+            getItemStub = sinon.stub().yields(err, null),
+            dbStub = { DynamoDB: function () { return {getItem: getItemStub}}};
+
+        var tags = proxyquire('../lib/tags', {'dynamodb-doc': dbStub});
+
+        tags.findById(req, res, done());
+		
+        expect(getItemStub.calledOnce).to.equal(true);
+        expect(sendSpy.calledOnce).to.equal(true);
+        expect(sendSpy.calledWith(500, err)).to.equal(true);
+    })
+})
 })
