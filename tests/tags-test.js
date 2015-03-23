38a5ae16d4978972ac128108de42657824be605d
msmith-bauer <matsmith@bauer-media.com.au>
2015-03-24 09:42:22 +1100

    refactored unit tests

--- tests/tags-test.js
+++ tests/tags-test.js
@@ -4,44 +4,63 @@ var proxyquire = require('proxyquire'),
     expect = chai.expect;
 
 describe('Tags', function () {
-	describe('FindById', function () {
-    it('should return a tag when passed a valid Id', function (done) {
-        var res = {},
-		    testId = "Test-Id",
-		    req = { params : { id : testId}}
-            sendSpy = res.send = sinon.spy(),
-            testTag = {"Item": { "Name": "tag2", "ID": testId, "Path": ["path1", "path2"] }},
-            getItemStub = sinon.stub().yields(null, testTag),
-            dbStub = { DynamoDB: function () { return {getItem: getItemStub}}};
+    var res, req, err, sendSpy, testId, testTag, getItemStub, dbStub;
 
-        var tags = proxyquire('../lib/tags', {'dynamodb-doc': dbStub});
+    beforeEach(function(done){
+        res = err = {},
+        testId = "Test-Id",
+        req = {params: {id: testId}},
+        sendSpy = res.send = sinon.spy(),
+        testTag = {"Item": {"Name": "tag2", "ID": testId, "Path": ["path1", "path2"]}},
+        dbStub = {
+            DynamoDB: function () {
+                return {getItem: getItemStub}
+            }
+        };
 
-        tags.findById(req, res, done());
-		
-        expect(getItemStub.calledOnce).to.equal(true);
-		expect(getItemStub.getCall(0).args[0].Key.ID).to.equal(testId);
-        expect(sendSpy.calledOnce).to.equal(true);
-        expect(sendSpy.calledWith(testTag)).to.equal(true);
+        done();
     })
-})
-})
 
-describe('Tags', function () {
-	describe('FindById', function () {
-    it('should return a 500 when passed an invalid Id', function (done) {
-        var res = err = {},
-		    req = { params : { id : "xxx"}}
-            sendSpy = res.send = sinon.spy(),
-            getItemStub = sinon.stub().yields(err, null),
-            dbStub = { DynamoDB: function () { return {getItem: getItemStub}}};
+    describe('FindById', function () {
+        it('should return a tag', function (done) {
+            getItemStub = sinon.stub().yields(null, testTag);
 
-        var tags = proxyquire('../lib/tags', {'dynamodb-doc': dbStub});
+            var tags = proxyquire('../lib/tags', {'dynamodb-doc': dbStub});
 
-        tags.findById(req, res, done());
-		
-        expect(getItemStub.calledOnce).to.equal(true);
-        expect(sendSpy.calledOnce).to.equal(true);
-        expect(sendSpy.calledWith(500, err)).to.equal(true);
+            tags.findById(req, res, done());
+
+            expect(getItemStub.calledOnce).to.equal(true);
+            expect(getItemStub.getCall(0).args[0].Key.ID).to.equal(testId);
+            expect(sendSpy.calledOnce).to.equal(true);
+            expect(sendSpy.calledWith(testTag)).to.equal(true);
+        })
     })
-})
+
+    describe('FindById', function () {
+        it('should return a 404 when id not found', function (done) {
+            getItemStub = sinon.stub().yields(null, {});
+
+            var tags = proxyquire('../lib/tags', {'dynamodb-doc': dbStub});
+
+            tags.findById(req, res, done());
+
+            expect(getItemStub.calledOnce).to.equal(true);
+            expect(sendSpy.calledOnce).to.equal(true);
+            expect(sendSpy.calledWith(404)).to.equal(true);
+        })
+    })
+
+    describe('FindById', function () {
+        it('should return a 500 when err', function (done) {
+            getItemStub = sinon.stub().yields(err, null);
+
+            var tags = proxyquire('../lib/tags', {'dynamodb-doc': dbStub});
+
+            tags.findById(req, res, done());
+
+            expect(getItemStub.calledOnce).to.equal(true);
+            expect(sendSpy.calledOnce).to.equal(true);
+            expect(sendSpy.calledWith(500, err)).to.equal(true);
+        })
+    })
 })
